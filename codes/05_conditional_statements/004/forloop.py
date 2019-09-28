98655e7c2d2fc988c5d345afacab032249479ba2
Jack Ngo <jngo@example.com>
2019-09-28 09:52:43 -0400

    for loop

---
+++ codes/05_conditional_statements/004/forloop.py
@@ -0,0 +1,18 @@
+# loop up index
+fruits = ['Banana', 'Apple', 'Groups']
+
+print('loop up via index value')
+print(len(fruits))
+for index in range(len(fruits)):
+    print(fruits[index])
+
+    list1 = [1, 2, 3, "Python"]
+
+print('loop up index')
+for i in list1:
+    print(i)
+
+# Loop Item
+print('loop up item')
+for fruit in fruits:
+    print(fruit)
---
+++ codes/docs/05_Conditional_Statements/004_for_loop.md
@@ -0,0 +1,9 @@
+## Syntax:
+
+```python
+for<variable>in <range>:
+    stmt1
+    stmt2
+    ...
+    stmtn
+```
